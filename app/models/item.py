f7ee075ef3cb2acd4c2013ce135dd94d3a0c92bc
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-08-15 14:42:37 +0300

    Add item shell
    Add the shell for the Item class from which methods shall be
    contructed

---
+++ app/models/item.py
@@ -0,0 +1,19 @@
+"""This module contains the Item class"""
+class Item(object):
+    """This class describes the structure of the Item object"""
+
+    def __init__(self, name, price, status=False):
+        """This method creates an instance of the Item class"""
+        pass
+
+    def change_status(self, new_status):
+        """This method changes the status in the Item instance"""
+        pass
+
+    def rename(self, name):
+        """This method renames the Item instance"""
+        pass
+
+    def change_price(self, price):
+        """This method changes the price of the Item instance"""
+        pass
