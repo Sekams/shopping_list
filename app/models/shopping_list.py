5b9a4d655e62590156bbc2dfd45450fcfb6ff065
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-08-15 14:25:41 +0300

    Add shopping_list shell
    Add the shell for the ShoppingList class from which methods shall be contructed

---
+++ app/models/shopping_list.py
@@ -0,0 +1,19 @@
+"""This module contains the ShoppingList class"""
+class ShoppingList(object):
+    """This class describes the structure of the ShoppingList object"""
+
+    def __init__(self, title, items={}):
+        """This method creates an instance of the ShoppingList class"""
+        pass
+
+    def add_item(self, item):
+        """This method creates an item in the ShoppingList instance"""
+        pass
+
+    def remove_item(self, item):
+        """This method removes an item from the ShoppingList instance"""
+        pass
+
+    def change_title(self, title):
+        """This method changes the title of the ShoppingList instance"""
+        pass
