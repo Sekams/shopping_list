470ca2a76e4c06387ac53afd217a90cfe8482c4e
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-08-15 16:03:38 +0300

    Add item methods
    Add methods and attributes to the Item class

--- app/models/item.py
+++ app/models/item.py
@@ -4,16 +4,21 @@ class Item(object):
 
     def __init__(self, name, price, status=False):
         """This method creates an instance of the Item class"""
-        pass
+        self.name = name
+        self.price = price
+        self.status = status
 
     def change_status(self, new_status):
         """This method changes the status in the Item instance"""
-        pass
+        self.status = new_status
+        return self.status
 
     def rename(self, name):
         """This method renames the Item instance"""
-        pass
+        self.name = name
+        return self.name
 
     def change_price(self, price):
         """This method changes the price of the Item instance"""
-        pass
+        self.price = price
+        return self.price
